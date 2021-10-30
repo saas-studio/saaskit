9740920e3787133c94be034d9387a807d0c8ff37
hackerhailee <93379124+hackerhailee@users.noreply.github.com>
2021-10-30 16:17:42 +0000

    Added some components

---
+++ packages/ui/src/Wordmark.tsx
@@ -0,0 +1,48 @@
+import React, { FC } from 'react'
+import { useGoogleFonts, GoogleFontsStatus } from "@flayyer/use-googlefonts";
+
+export interface Props {
+  name?: string,
+  fontFamily?: string,
+  nameLeft?: string,
+  nameRight?: string,
+  fontLeft?: string,
+  fontRight?: string,
+}
+
+export const Wordmark: FC<Props> = ({name = 'SaaS.Dev', fontFamily = 'Cabin'}) => {
+    const font = useGoogleFonts([
+        {
+            family: fontFamily, // Family Name
+            styles: [
+            "600..700", // Range, if family supports it.
+            "100..200italic", // Range with italic
+            "300italic", // Weight with italic
+            "regular", // Shortcut to 400
+            "italic", // Shortcut to 400 Italic
+            "500", // Regular with weight
+            444, // Regular weight for variable font
+            ],
+        },
+        {
+            family: "Roboto", // Family Name - Roboto doesn't support ranges
+            styles: [
+            "300italic", // Weight with italic
+            "regular", // Shortcut to 400
+            "italic", // Shortcut to 400 Italic
+            "500",
+            100,
+            ],
+        },
+    ]);
+
+    if (font.status === GoogleFontsStatus.FAILED) {
+        console.log(font.error);
+    }
+
+  return (
+    <h1 className="text-gray-300" style={{ fontFamily: `'${fontFamily}', sans-serif` }}>
+        {name}
+    </h1>
+  )
+};
---
+++ packages/ui/stories/Wordmark.stories.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Meta, Story } from '@storybook/react';
+import { Wordmark, Props } from '../src/Wordmark';
+
+const meta: Meta = {
+  title: 'Wordmark',
+  component: Wordmark,
+  argTypes: {
+    name: {
+      control: {
+        type: 'text',
+      },
+    },
+    fontFamily: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
+  parameters: {
+    controls: { expanded: true },
+  },
+};
+
+export default meta;
+
+const Template: Story<Props> = args => <Wordmark {...args} />;
+
+export const Default = Template.bind({});
+
+Default.args = {};
