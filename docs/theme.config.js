a03c5c926a133062529014c3769e8e3409c1ec47
hackerhailee <93379124+hackerhailee@users.noreply.github.com>
2021-10-30 16:42:36 +0000

    Added docs

---
+++ docs/theme.config.js
@@ -0,0 +1,67 @@
+export default {
+  github: 'https://github.com/saas-studio/saaskit',
+  docsRepositoryBase: 'https://github.com/saas-studio/saaskit/blob/master',
+  titleSuffix: ' – SaaSkit',
+  logo: (
+    <>
+      <span className="mr-2 font-extrabold hidden md:inline">SaaSkit</span>
+      <span className="text-gray-600 font-normal hidden md:inline">
+        Rapid SaaS Development Framework
+      </span>
+    </>
+  ),
+  head: (
+    <>
+      <meta name="msapplication-TileColor" content="#ffffff" />
+      <meta name="theme-color" content="#ffffff" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta httpEquiv="Content-Language" content="en" />
+      <meta name="description" content="SaaSkit: Rapid SaaS Development Framework" />
+      <meta name="og:description" content="SaaSkit: Rapid SaaS Development Framework" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:image" content="https://saaskit.js.org/og.png" />
+      <meta name="twitter:site:domain" content="saaskit.js.org" />
+      <meta name="twitter:url" content="https://saaskit.js.org" />
+      <meta name="og:title" content="SaaSkit: Rapid SaaS Development Framework" />
+      <meta name="og:image" content="https://saaskit.js.org/og.png" />
+      <meta name="apple-mobile-web-app-title" content="SaaSkit" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-icon-180x180.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="192x192"
+        href="/android-icon-192x192.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="96x96"
+        href="/favicon-96x96.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+    </>
+  ),
+  search: true,
+  prevLinks: true,
+  nextLinks: true,
+  footer: true,
+  footerEditLink: 'Edit this page on GitHub',
+  footerText: <>MIT {new Date().getFullYear()} © <a href="https://saas.dev">SaaS.Dev</a></>,
+  unstable_faviconGlyph: '👋',
+}
