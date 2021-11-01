411336f240a57894ffe0b7648bff2d4537af32dc
hackerhailee <hailee@saas.dev>
2021-11-01 05:01:56 -0500

    Updated some package references

--- templates/site/pages/api/auth/[...Auth].js
+++ templates/site/pages/api/auth/[...Auth].js
@@ -1,11 +1,11 @@
-import Auth from '@saaskit/auth'
-import GithubProvider from '@saaskit/auth/providers/github'
+// import Auth from '@saaskit/api'
+// import GithubProvider from '@saaskit/api/providers/github'
 
-export default Auth({
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
-  ],
-})
+// export default Auth({
+//   providers: [
+//     GithubProvider({
+//       clientId: process.env.GITHUB_ID,
+//       clientSecret: process.env.GITHUB_SECRET,
+//     }),
+//   ],
+// })
