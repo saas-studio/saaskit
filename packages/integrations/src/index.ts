0f362817a94df2864754dbd5a2095ff29b203e59
hackerhailee <hailee@saas.dev>
2021-11-01 07:48:49 -0500

    Updates to some versions

--- packages/integrations/src/index.ts
+++ packages/integrations/src/index.ts
@@ -1,6 +1,5 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
-  }
-  return a + b;
-};
+// import Zapier from '@saaskit/zapier-marketing'
+
+export default {
+  zapier: {} //Zapier
+}
